1c360305d1e567f03a1b053468b72710024e6312
rafalwawrzyk <rafal.wawrzyk@gmail.com>
2017-05-09 15:23:54 +0200

    menu and loading page
    add menu actions and loading page

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(function(){
+    
+    $(document).on('click', 'a', function(){
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop:$($.attr(this, 'href')).offset().top - 150
+        },500)
+    })
+    
+    
+    $(function scroll(){
+         var $navbar = $('.row-nav').offset().top;
+            $(window).scroll(function(){
+            if($(window).scrollTop() >= $navbar){
+              $('.row-nav').addClass('navigation-row')
+            }else{
+                $('.row-nav').removeClass('navigation-row');
+            }
+        })
+    });
+    
+     var $overlay = $('.loading-page');
+
+    $(window).on('load', function () {
+        $overlay.hide();
+    });
+   
+   
+    
+    
+    
+})
+    
+    
+    
+    
